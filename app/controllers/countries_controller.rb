625f3193c0edabccf71113b3ab0fbeeebd6ca17a
Carolina Sato <carol.sato@gmail.com>
2015-11-28 15:16:11 -0200

    Editar

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -5,4 +5,16 @@ class CountriesController < ApplicationController
 	def show
 		@country = Country.find(params[:id])
 	end
+	def edit 
+		@country = Country.find(params[:id])
+    end
+    def update
+    	@country = Country.find(params[:id])
+    	@country.update_attributes(country_attributes)
+    	redirect_to :action => :show
+    end
+    def country_attributes
+    	params.require(:country).permit!
+    end
+
 end
